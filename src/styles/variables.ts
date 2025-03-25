0038c1440755ea8edc965bd6358bef2a8c2d919c
bruna <bruna@teste.com>
2025-03-25 19:53:36 -0300

    initial commit

---
+++ src/styles/variables.ts
@@ -0,0 +1,7 @@
+export const variables = {
+    colorHeader: "#C37670",
+    backgroundSections: "#79383A",
+    colorText: "#AC5452",
+    primaryColor: "#E1E1E1",
+    secondaryColor: "#E1E1E1",
+}
