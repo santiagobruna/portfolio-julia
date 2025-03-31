9cbe0c65788447ae9a2fcd3b34eba0afa9f00ca8
bruna <bruna@teste.com>
2025-03-31 17:58:38 -0300

    corrigindo caminho dos links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -40,8 +40,8 @@ const Header = () => {
                 <div className={`lg:hidden ${isOpen ? "block" : "hidden"}`}>
                     <ul className="flex flex-col gap-4 ">
                         <li><S.NavLink href='#medica'>Médica Pediatra</S.NavLink></li>
-                        <li><S.NavLink href='#recursos'>Artigos e pesquisas</S.NavLink></li>
-                        <li><S.NavLink href='#artigos'>Recursos</S.NavLink></li>
+                        <li><S.NavLink href='#artigos'>Artigos e pesquisas</S.NavLink></li>
+                        <li><S.NavLink href='#recursos'>Recursos</S.NavLink></li>
                         <li><S.NavLink href='#convenios'>Convênios</S.NavLink></li>
                         <li><S.NavLink href='#depoimentos'>Depoimentos</S.NavLink></li>
                         <li><S.NavLink href='#contato'>Contato</S.NavLink></li>
